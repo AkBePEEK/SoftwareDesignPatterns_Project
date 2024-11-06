b1becae49d412b8e5722c83027712d2fe5c81f15
AkBe <154293625+AkBePEEK@users.noreply.github.com>
2024-11-06 16:55:54 +0500

    feat: Add Main class for checking the Patterns

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import BehavioralPattern.ObserverPattern.*;
+import BehavioralPattern.StatePattern.*;
+import CreationalPattern.BuilderPattern.*;
+import CreationalPattern.PrototypePattern.*;
+import StructuralPattern.CompositePattern.*;
+import StructuralPattern.CompositePattern.Module;
+import StructuralPattern.Decorator.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        StructuralPattern. Decorator. Course course1 = new BasicCourse();
+        StructuralPattern. Decorator. Course courseWithQuiz = new QuizDecorator(course1);
+        StructuralPattern. Decorator. Course courseWithCertificate = new CertificateDecorator(courseWithQuiz);
+        System.out.println(courseWithCertificate.getDetails());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Enrollment enrollment = new Enrollment();
+        enrollment.proceed(); // Transition to Active
+        enrollment.proceed(); // Transition to Completed
+
+        Course_BehavioralObserverPattern course = new Course_BehavioralObserverPattern();
+        User user1 = new User("Alice");
+        User user2 = new User("Bob");
+
+        course.addSubscriber(user1);
+        course.addSubscriber(user2);
+        course.notifySubscribers("New assignment available!");
+
+        Module module = new Module("Programming Basics");
+        module.addLesson(new Lesson("Lesson 1: Variables"));
+        module.addLesson(new Lesson("Lesson 2: Loops"));
+        module.showContent();
+
+        Course_BuilderPattern course2 = new CourseBuilder()
+                .addModule("Introduction Module")
+                .addQuiz("Quiz 1")
+                .addAssignment("Assignment 1")
+                .build();
+        System.out.println(course2);
+
+        try {
+            CourseModule_PrototypePattern baseModule = new CourseModule_PrototypePattern("Introduction", "Welcome to the course!");
+            CourseModule_PrototypePattern clonedModule = baseModule.clone();
+            System.out.println(clonedModule);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
         }
     }
 }
