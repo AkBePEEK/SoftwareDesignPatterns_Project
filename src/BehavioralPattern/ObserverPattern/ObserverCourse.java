7e4029d6dd26e8a00947f71c094a89f739845f17
AkBe <154293625+AkBePEEK@users.noreply.github.com>
2024-11-06 16:13:52 +0500

    fix: Add ObserverCourse class

---
+++ src/BehavioralPattern/ObserverPattern/ObserverCourse.java
@@ -0,0 +1,22 @@
+package BehavioralPattern.ObserverPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObserverCourse {
+    private final List<Observer> subscribers = new ArrayList<>();
+
+    public void addSubscriber(Observer observer) {
+        subscribers.add(observer);
+    }
+
+    public void removeSubscriber(Observer observer) {
+        subscribers.remove(observer);
+    }
+
+    public void notifySubscribers(String message) {
+        for (Observer observer : subscribers) {
+            observer.update(message);
+        }
+    }
+}
